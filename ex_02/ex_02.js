ab29893e9bd583c1264debe7292430b298859e6e
rosa-skn <rosa.sokun@epitech.digital>
2024-10-15 23:59:29 +0200

    Create ex_02.js

---
+++ ex_02/ex_02.js
@@ -0,0 +1,16 @@
+document.getElementById('Data').addEventListener('click', () => {
+    const url = 'GLB.Ts+dSST.csv'; //j'arrive pas a le faire marcher, je pense que c'est a cause de la serveur qui bloque, j'ai testé avec le url du doc aussi le convertir en json mais aucun de ces methodes marchent
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error: ' + response.status);
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('dataDisplay').innerText = data;
+        })
+        .catch(error => {
+            document.getElementById('dataDisplay').innerText = 'Error: ' + error.message;
+        });
+});
