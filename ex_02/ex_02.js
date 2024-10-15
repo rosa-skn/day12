7ec41cbc752c3ec91978d1fa1bebebb92744a6de
rosa-skn <rosa.sokun@epitech.digital>
2024-10-16 00:02:23 +0200

    Update ex_02.js

--- ex_02/ex_02.js
+++ ex_02/ex_02.js
@@ -1,5 +1,5 @@
 document.getElementById('Data').addEventListener('click', () => {
-    const url = 'GLB.Ts+dSST.csv'; //j'arrive pas a le faire marcher, je pense que c'est a cause de la serveur qui bloque, j'ai testé avec le url du doc aussi le convertir en json mais aucun de ces methodes marchent
+    const url = 'GLB.Ts+dSST.csv'; //j'arrive pas a le faire marcher, je pense que c'est a cause de la serveur qui bloque, j'ai testé avec le url du doc aussi le convertir en json mais aucun de ces methodes marchent. ici j'ai  lié le csv avec mon js. 
     fetch(url)
         .then(response => {
             if (!response.ok) {
